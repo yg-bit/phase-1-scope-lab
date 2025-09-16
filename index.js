83f0bbc9c32f8f99692b9dddf247c01c515ec1b8
yg-bit <jamesisaiah164@gmail.com>
2025-09-16 12:07:02 +0300

    added

--- index.js
+++ index.js
@@ -5,8 +5,7 @@ const leastFavoriteCustomer = 'some initial value'
 function upperCaseCustomerName() {
   customerName = customerName.toUpperCase()
   
-  //notice that there is no return statement
-  //if you try to use `return customerName.toUpperCase()`, it will return 'BOB', but will not redefine the variable!
+
 }
 
 function setBestCustomer() {
